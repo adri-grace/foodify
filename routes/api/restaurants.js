e9b848d659ab6793762c2e8d466ab04c3936ff27
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-03-04 16:34:26 -0800

    add authentication

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const restaurantCtrl = require('../../controllers/restaurants');
+const isAuthenticated = require('../../config/auth');
 
-router.post('/', restaurantCtrl.create);
-router.get('/', restaurantCtrl.index);
+router.post('/', isAuthenticated, restaurantCtrl.create);
+router.get('/', isAuthenticated, restaurantCtrl.index);
 router.get('/featured', restaurantCtrl.getFeatured);
 
 module.exports = router;
